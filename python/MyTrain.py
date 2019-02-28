4251e4268bdb46eedfa382530b14aa7788bdb414
Zhangxing Bian <jasonbian.zx@outlook.com>
2019-02-28 12:07:09 +0800

    common
    Delete original dice_loss.py and add into add metrics.py.

---
+++ python/MyTrain.py
@@ -0,0 +1,223 @@
+import sys
+import os
+from optparse import OptionParser
+import numpy as np
+import time
+from torch.optim import lr_scheduler
+import torch
+import torch.backends.cudnn as cudnn
+import torch.nn as nn
+from torch import optim
+# customized import
+# from [filename] import [function name]
+from .load import *
+from .utils import split_train_val, batch
+from .fcn import *
+from .metrics import *
+###########################################################################
+#                               Train                                     #
+###########################################################################
+
+def train_net(net,
+              epochs=5,
+              batch_size=1,
+              lr=0.1,
+              val_percent=0.1,
+              save_cp=True,
+              gpu=True,
+              img_scale=1.,
+              dir_img="./",
+              dir_mask="./",
+              dir_checkpoint="./"):
+    momentum   = 0
+    w_decay    = 1e-5
+    step_size  = 50
+    gamma      = 0.5
+    # return only the file name without extension.
+    ids = get_ids(dir_img)
+    if len(ids)==0:
+        print("dir_img is not valid")
+    # generate tuples like (id,#)
+    ids = split_ids(ids)
+    # split into train and val w.r.t val_percent
+    iddataset = split_train_val(ids, val_percent)
+    # show configeration info
+    print('''
+        Starting training:
+            Epochs: {}
+            Batch size: {}
+            Learning rate: {}
+            Training size: {}
+            Validation size: {}
+            Checkpoints: {}
+            CUDA: {}
+        '''.format(epochs, batch_size, lr, len(iddataset['train']),
+                   len(iddataset['val']), str(save_cp), str(gpu)))
+    # use N_train to show where the training process is
+    N_train = len(iddataset['train'])
+    # set optimizer and criterion for loss
+    criterion = nn.BCEWithLogitsLoss()
+    optimizer = optim.RMSprop(fcn_model.parameters(), lr=lr, momentum=momentum, weight_decay=w_decay)
+    scheduler = lr_scheduler.StepLR(optimizer, step_size=step_size, gamma=gamma)  # decay LR by a factor of 0.5 every 30 epochs
+
+    # start epoch loop
+    for epoch in range(epochs):
+        scheduler.step()
+        print('Starting epoch {}/{}.'.format(epoch + 1, epochs))
+        # set net mode to "train"
+        net.train()
+        # preprocess image in your dataset/dir, including:
+        # 1. resize and crop
+        # 2. transform image from HWC to CHW
+        # 3. normalize images e.g. img/255.
+        # 4. convert GT image into binary
+        train = get_imgs_and_masks(iddataset['train'], dir_img, dir_mask, img_scale)
+        val = get_imgs_and_masks(iddataset['val'], dir_img, dir_mask, img_scale)
+        #
+        epoch_loss = 0
+        # start batch loop
+        for i, b in enumerate(batch(train, batch_size)):
+            # generate tensor batch
+            imgs_list=[]
+            mask_list=[]
+            for k in b:
+                imgs_list.append(k[0])
+                mask_list.append(k[1])
+            imgs = np.array(imgs_list).astype(np.float32)
+            true_masks = np.array(mask_list).astype(np.float32)
+            imgs = torch.from_numpy(imgs)
+            true_masks = torch.from_numpy(true_masks)
+            # The shape here is (Batchsize,C,W,H)
+            # Load to training data to GPU
+            if gpu:
+                imgs = imgs.cuda()
+                true_masks = true_masks.cuda()
+            # inference once
+            masks_pred = net(imgs)
+            # flatten 4D matrix into 1D for computation simplicity
+            masks_probs_flat = masks_pred.view(-1)
+            true_masks_flat = true_masks.view(-1)
+            # calculate loss and accumulate loss of each batch to calculate epoch loss
+            loss = criterion(masks_probs_flat, true_masks_flat)
+            epoch_loss += loss.item()
+            # show where the training process is
+            print('{0:.4f} --- loss: {1:.6f}'.format(i * batch_size / N_train, loss.item()))
+            # clear the place for new grad
+            optimizer.zero_grad()
+            # BP
+            loss.backward()
+            # update optimizer to possibly change learning rate
+            optimizer.step()
+        # show loss os this Epoch
+        print('Epoch finished ! Loss: {}'.format(epoch_loss / i))
+
+        # test model using val set
+        val_dice = eval_net(net, val, gpu)
+        print('Validation Dice Coeff: {}'.format(val_dice))
+        # save model in dir_checkpoint
+        if save_cp:
+            torch.save(net.state_dict(),
+                       dir_checkpoint + 'CP{}.pth'.format(epoch + 1))
+            print('Checkpoint {} saved !'.format(epoch + 1))
+
+###########################################################################
+#                               Evaluate                                  #
+###########################################################################
+
+def eval_net(net, dataset, gpu=True):
+    # set model to evaluation model
+    net.eval()
+    tot = 0
+    tiou=0
+    for i, b in enumerate(dataset):
+        img = b[0]
+        true_mask = b[1]
+        img = torch.from_numpy(img).unsqueeze(0)
+        true_mask = torch.from_numpy(true_mask).unsqueeze(0)
+        # The shape here is (1,1,W,H)
+        if gpu:
+            img = img.cuda()
+            true_mask = true_mask.cuda()
+
+        mask_pred = net(img)[0]
+        mask_pred = (mask_pred > 0.5).float()
+
+        # compute coefficient value
+        tot += dice_coeff(mask_pred, true_mask).item()
+        tiou += iou(mask_pred, true_mask,2)[1]
+
+    return (tot / i,tiou/i)
+
+###########################################################################
+#                               get_args                                  #
+###########################################################################
+def get_args():
+    parser = OptionParser()
+    parser.add_option('-e', '--epochs', dest='epochs', default=20, type='int',
+                      help='number of epochs')
+    parser.add_option('-b', '--batch-size', dest='batchsize', default=10,
+                      type='int', help='batch size')
+    parser.add_option('-l', '--learning-rate', dest='lr', default=0.01,
+                      type='float', help='learning rate')
+    parser.add_option('-g', '--gpu', action='store_true', dest='gpu',
+                      default=True, help='use cuda')
+    parser.add_option('-c', '--load', dest='load',
+                      default=False, help='load file model')
+    parser.add_option('--n_class', dest='n_class', type='int',
+                      default=2, help='n_class')
+    # parser.add_option('-s', '--scale', dest='scale', type='float',
+    #                   default=1., help='downscaling factor of the images')
+    # Directory
+    parser.add_option('--dir_img', dest='dir_img', type='string',
+                      default='/home/jasonbian/Desktop/WholeDataSet/DataSet1.0/cropped/')
+    parser.add_option('--dir_mask', dest='dir_mask', type='string',
+                      default='/home/jasonbian/Desktop/WholeDataSet/DataSet1.0/BinaryMask3/')
+    parser.add_option('-dir_checkpoint', dest='dir_checkpoint', type='string',
+                      default='./checkpoints/')
+
+    (options, args) = parser.parse_args()
+    return options
+
+###########################################################################
+#                               Main                                      #
+###########################################################################
+if __name__ == '__main__':
+
+    # get global parameters: including: epochs,batchsize,lr,gpu
+    args = get_args()
+    # generate Model instance
+    vgg_model = VGGNet(requires_grad=True, remove_fc=True)
+    fcn_model = FCNs(pretrained_net=vgg_model, n_class=args.n_class)
+    # read global paprameters and do some preparation
+    if args.load:
+        fcn_model.load_state_dict(torch.load(args.load))
+        print('Model loaded from {}'.format(args.load))
+
+    if args.gpu:
+        ts = time.time()
+        vgg_model = vgg_model.cuda()
+        fcn_model = fcn_model.cuda()
+        num_gpu = list(range(torch.cuda.device_count()))
+        fcn_model = nn.DataParallel(fcn_model, device_ids=num_gpu)
+        print("Finish cuda loading, time elapsed {}".format(time.time() - ts))
+
+    # read args and train Model
+    try:
+        train_net(net=fcn_model,
+                  epochs=args.epochs,
+                  batch_size=args.batchsize,
+                  lr=args.lr,
+                  gpu=args.gpu,
+                  img_scale=args.scale,
+                  dir_img=args.dir_img,
+                  dir_mask=args.dir_mask,
+                    dir_checkpoint=args.dir_checkpoint)
+    #  save interrupt model
+    except KeyboardInterrupt:
+        torch.save(fcn_model.state_dict(), 'INTERRUPTED.pth')
+        print('Saved interrupt')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
+
---
+++ python/metrics.py
@@ -0,0 +1,62 @@
+
+def iou(pred, target,n_class):
+    ious = []
+    for cls in range(n_class):
+        pred_inds = pred == cls
+        target_inds = target == cls
+        intersection = pred_inds[target_inds].sum()
+        union = pred_inds.sum() + target_inds.sum() - intersection
+        if union == 0:
+            ious.append(float('nan'))  # if there is no ground truth, do not include in evaluation
+        else:
+            ious.append(float(intersection) / max(union, 1))
+        # print("cls", cls, pred_inds.sum(), target_inds.sum(), intersection, float(intersection) / max(union, 1))
+    return ious
+
+
+def pixel_acc(pred, target):
+    correct = (pred == target).sum()
+    total   = (target == target).sum()
+    return correct / total
+
+import torch
+from torch.autograd import Function, Variable
+
+class DiceCoeff(Function):
+    """Dice coeff for individual examples"""
+
+    def forward(self, input, target):
+        self.save_for_backward(input, target)
+        eps = 0.0001
+        self.inter = torch.dot(input.view(-1), target.view(-1))
+        self.union = torch.sum(input) + torch.sum(target) + eps
+
+        t = (2 * self.inter.float() + eps) / self.union.float()
+        return t
+
+    # This function has only a single output, so it gets only one gradient
+    def backward(self, grad_output):
+
+        input, target = self.saved_variables
+        grad_input = grad_target = None
+
+        if self.needs_input_grad[0]:
+            grad_input = grad_output * 2 * (target * self.union - self.inter) \
+                         / (self.union * self.union)
+        if self.needs_input_grad[1]:
+            grad_target = None
+
+        return grad_input, grad_target
+
+
+def dice_coeff(input, target):
+    """Dice coeff for batches"""
+    if input.is_cuda:
+        s = torch.FloatTensor(1).cuda().zero_()
+    else:
+        s = torch.FloatTensor(1).zero_()
+
+    for i, c in enumerate(zip(input, target)):
+        s = s + DiceCoeff().forward(c[0], c[1])
+
+    return s / (i + 1)
